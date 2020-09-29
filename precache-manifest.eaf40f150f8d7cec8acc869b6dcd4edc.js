9da94278a66003c7c3f6c1d4f1234a9594020116
matute <tosomatias@gmail.com>
2020-09-29 18:07:04 -0300

    Updates

---
+++ precache-manifest.eaf40f150f8d7cec8acc869b6dcd4edc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21fb9882c54aa777b0856fb2064ad595",
+    "url": "/Buscador-Bebidas/index.html"
+  },
+  {
+    "revision": "99a2b71f21ff270916f8",
+    "url": "/Buscador-Bebidas/static/css/main.15089814.chunk.css"
+  },
+  {
+    "revision": "2c7b8d66825430a56f78",
+    "url": "/Buscador-Bebidas/static/js/2.4821d250.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/Buscador-Bebidas/static/js/2.4821d250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a2b71f21ff270916f8",
+    "url": "/Buscador-Bebidas/static/js/main.16746300.chunk.js"
+  },
+  {
+    "revision": "4ce181fe31bb0e1d47b6",
+    "url": "/Buscador-Bebidas/static/js/runtime-main.8e9ba864.js"
+  }
+]);
